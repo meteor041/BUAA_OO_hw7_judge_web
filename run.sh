1b489482c01ce9aea3693893b0423085b3f38335
刘鑫宇 <23371510@buaa.edu.cn>
2025-03-28 18:33:42 +0800

    修改打印语句bug

--- run.sh
+++ run.sh
@@ -31,59 +31,57 @@ for i in $(seq 1 $NUM_ITERATIONS); do
     fi
 
  
-# 遍历 jar 文件
-for jar_file in "$PROGRAM_DIR"/*.jar; do
-  if [ ! -f "$jar_file" ]; then
- 	   continue  # 如果不是文件就跳过
-  fi
-	 jar_filename=$(basename "$jar_file")
-echo "Processing jar file: $jar_filename"
-  jar_filename_noext="${jar_filename%.*}"  # 去掉 .jar 后缀
-	
-  timestamp=$(date "+%m-%d-%H-%M-%S")
-  log_dir="log/$jar_filename_noext/$timestamp"
-#  log_dir=$(readlink -f "log_dir")
-  mkdir -p "$log_dir"
-  touch "$log_dir/result.txt"
+    # 遍历 jar 文件
+    for jar_file in "$PROGRAM_DIR"/*.jar; do
+        if [ ! -f "$jar_file" ]; then
+           continue  # 如果不是文件就跳过
+        fi
+         jar_filename=$(basename "$jar_file")
+        echo "Processing jar file: $jar_filename"
+        jar_filename_noext="${jar_filename%.*}"  # 去掉 .jar 后缀
 
-  current_concurrent=0
-  result=true
+        timestamp=$(date "+%m-%d-%H-%M-%S")
+        log_dir="log/$jar_filename_noext/$timestamp"
+        mkdir -p "$log_dir"
+        touch "$log_dir/result.txt"
 
-  
-    cat stdin.txt > $log_dir/input$i.txt 
-    # 执行 Java 程序  (后台运行) &
-    # -Xms 和 -Xmx 控制堆内存
-    # -XX:MetaspaceSize 和 -XX:MaxMetaspaceSize 控制元空间 (Metaspace) 大小
-    # timeout 用于限制总的运行时间
-    #使用./datainput_student_linux_x86_64 作为输入
-    timeout 100s ./datainput_student_linux_x86_64 | java -Xms512m -Xmx768m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=128m  -jar "$jar_file" > "$log_dir/output$i.txt" 2>> "$log_dir/result.txt" &
+        current_concurrent=0
+        result=true
 
-    # 控制并发进程数
-    current_concurrent=$((current_concurrent + 1))
-    if [[ $current_concurrent -ge $MAX_CONCURRENT ]]; then
-      wait  # 等待所有后台进程完成
-      current_concurrent=0
-    fi
-	PID=$!  # 获取上一个后台命令的进程 ID
-	wait $PID  # 等待 PID 对应的进程结束
-	echo -n "iteration $i : " >> "$log_dir/result.txt"
-    # 分析结果和计算得分
-    python3 judge/judge.py --input_file="$log_dir/input$i.txt" --output_file="$log_dir/output$i.txt" >> "$log_dir/result.txt"
-    return_code=$?
-    if [ $return_code -ne 0 ]; then
-      echo "  Error: Python judge script failed with return code: $return_code"
-      result=false
-    fi
 
-    python3 judge/score.py --input_file="$log_dir/input$i.txt" --output_file="$log_dir/output$i.txt" >> "$log_dir/result.txt"
-    echo "  ----------------------------------------" >> "$log_dir/result.txt"
+        cat stdin.txt > $log_dir/input$i.txt
+        # 执行 Java 程序  (后台运行) &
+        # -Xms 和 -Xmx 控制堆内存
+        # -XX:MetaspaceSize 和 -XX:MaxMetaspaceSize 控制元空间 (Metaspace) 大小
+        # timeout 用于限制总的运行时间
+        #使用./datainput_student_linux_x86_64 作为输入
+        timeout 100s ./datainput_student_linux_x86_64 | java -Xms512m -Xmx768m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=128m  -jar "$jar_file" > "$log_dir/output$i.txt" 2>> "$log_dir/result.txt" &
 
-  done
+        # 控制并发进程数
+        current_concurrent=$((current_concurrent + 1))
+        if [[ $current_concurrent -ge $MAX_CONCURRENT ]]; then
+          wait  # 等待所有后台进程完成
+          current_concurrent=0
+        fi
+        PID=$!  # 获取上一个后台命令的进程 ID
+        wait $PID  # 等待 PID 对应的进程结束
+        echo -n "iteration $i : " >> "$log_dir/result.txt"
+        # 分析结果和计算得分
+        python3 judge/judge.py --input_file="$log_dir/input$i.txt" --output_file="$log_dir/output$i.txt" >> "$log_dir/result.txt"
+        return_code=$?
+        if [ $return_code -ne 0 ]; then
+          echo "  Error: Python judge script failed with return code: $return_code"
+          result=false
+        fi
 
-  wait # 等待所有后台进程完成
+        python3 judge/score.py --input_file="$log_dir/input$i.txt" --output_file="$log_dir/output$i.txt" >> "$log_dir/result.txt"
+        echo "  ----------------------------------------" >> "$log_dir/result.txt"
+        echo "$log_dir : $result"
+    done
 
-  echo "$log_dir : $result"
-  echo "Finished processing $jar_filename"
+    wait # 等待所有后台进程完成
+
+    echo "Finished processing $jar_filename"
 
 done
 
