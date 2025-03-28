19c634980e93cf9d06e3cd691c318121b230ac59
刘鑫宇 <23371510@buaa.edu.cn>
2025-03-28 13:39:42 +0800

    修改端口

--- app.py
+++ app.py
@@ -284,4 +284,4 @@ def format_size(size):
     return f"{size:.2f} TB"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080) 
+    app.run(debug=True, host='0.0.0.0', port=8082)
